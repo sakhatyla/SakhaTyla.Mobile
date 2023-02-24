24341a1035b0e7a535e79cec4336b89b103cff58
Vassily Ivanov <vivanov@sharplogic.ru>
2023-02-24 22:10:42 +0900

    Add Google Analytics #11

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sakhatyla/common/theme.dart';
+import 'package:sakhatyla/firebase_options.dart';
 import 'package:sakhatyla/locator.dart';
 import 'package:sakhatyla/main/main.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   setupLocator();
   runApp(MyApp());
 }
